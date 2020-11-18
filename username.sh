f8a750fd752ca313d9403226cdfc0e216b05cd8b
Alan Lu <allu@chapman.edu>
2020-11-17 20:49:51 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -3,10 +3,13 @@
 # Alan Lu
 echo "Enter a username: "
 read NAME
-while echo "$NAME" | egrep -E -v "^[a-z][0-9]{3,12}$" > /dev/null 2>&1
+while echo "$NAME" | egrep -E -v "^[a-z][0-9a-z_]{2,12}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid Username!"
+	echo "Between 3 and 12 characters"
+	echo "Starts with a lowercase letter"
+	echo "Can only use lower case letters, digits, and underscores"
 	echo "Enter a Username:"
-	read NAME
+	read -r NAME
 done
 echo "Thank you"
